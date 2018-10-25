46f0c1e464762dd9417d17c16663679304801c1c
ntdat99 <datntd00498@fpt.edu.vn>
2018-10-25 09:23:54 +0700

    weather

---
+++ APIManager.cs
@@ -0,0 +1,127 @@
+﻿   using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppDemo
+{
+    public class APIManager
+    {
+        public async static Task<RootObject> getWeather(double lat, double lon)
+        {
+            var http = new HttpClient();
+            var url = String.Format("http://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&units=metric&appid=96381a872b1b405c5bf83b2ed63d9561",lat,lon);
+            var response = await http.GetAsync(url); // nhận data  json từ weathermap
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+            // Khởi tạo Stream để đọc json
+            var ms= new MemoryStream(Encoding.UTF8.GetBytes(result));
+            // đọc object đã phân tích được từ json vào stream local để phân tích
+            var data = (RootObject)serializer.ReadObject(ms);
+            return data;
+        }
+    }
+
+    [DataContract]
+    public class Coord
+    {
+        [DataMember]
+        public double lon { get; set; }
+        [DataMember]
+        public int lat { get; set; }
+    }
+
+    [DataContract]
+    public class Weather
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string main { get; set; }
+        [DataMember]
+        public string description { get; set; }
+        [DataMember]
+        public string icon { get; set; }
+    }
+
+    [DataContract]
+    public class Main
+    {
+        [DataMember]
+        public int temp { get; set; }
+        [DataMember]
+        public int pressure { get; set; }
+        [DataMember]
+        public int humidity { get; set; }
+        [DataMember]
+        public int temp_min { get; set; }
+        [DataMember]
+        public int temp_max { get; set; }
+    }
+
+    [DataContract]
+    public class Wind
+    {
+        [DataMember]
+        public double speed { get; set; }
+        [DataMember]
+        public int deg { get; set; }
+    }
+
+    [DataContract]
+    public class Clouds
+    {
+        [DataMember]
+        public int all { get; set; }
+    }
+
+    [DataContract]
+    public class Sys
+    {
+        [DataMember]
+        public int type { get; set; }
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public double message { get; set; }
+        [DataMember]
+        public string country { get; set; }
+        [DataMember]
+        public int sunrise { get; set; }
+        [DataMember]
+        public int sunset { get; set; }
+    }
+
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public Coord coord { get; set; }
+        [DataMember]
+        public List<Weather> weather { get; set; }
+        [DataMember]
+        public string @base { get; set; }
+        [DataMember]
+        public Main main { get; set; }
+        [DataMember]
+        public int visibility { get; set; }
+        [DataMember]
+        public Wind wind { get; set; }
+        [DataMember]
+        public Clouds clouds { get; set; }
+        public int dt { get; set; }
+        [DataMember]
+        public Sys sys { get; set; }
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public int cod { get; set; }
+    }
+}
